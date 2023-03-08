c59adca85e051a5f0f84aba1a694a25cda8bc162
Mursalin-Dowla <justjim844@gmail.com>
2023-03-09 03:44:37 +0600

    sign in with google added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,44 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword, onAuthStateChanged, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import auth from "../../firebase.init";
 
 const Login = () => {
+ const [myError,setMyError] = useState();
+ const [user,setUser] = useState({});
+ const navigate = useNavigate();
+ const provider = new GoogleAuthProvider();
+
+ useEffect(()=>{
+  onAuthStateChanged(auth, user=>{
+      setUser(user);
+  })
+},[])
+if(user){
+  navigate('/');
+}
+  const handleFormSubmit = (event)=>{
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+       signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+            })
+            .catch((error) => {
+              const errorMessage = error.message;
+              setMyError(errorMessage);
+            });     
+}
+const handleGoogleSignIn =()=>{
+  signInWithPopup(auth,provider)
+  .then(result=>{
+    // const myUser = result.user;
+    
+    // setUser(myUser);
+ })
+}
   return (
     <>
       <section className="container mt-20 mx-auto  min-h-screen my-font">
@@ -14,19 +49,20 @@ const Login = () => {
             </h1>
           </div>
           <div className="w-full mt-10 mx-auto max-w-xs md:max-w-md">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <form onSubmit={handleFormSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
               <div className="mb-4">
                 <label
                   className="block text-red-400 text-sm md:text-lg font-bold mb-2"
                   for="username"
                 >
-                  Username
+                  Email
                 </label>
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="username"
+                  id="email"
                   type="text"
-                  placeholder="Username"
+                  name="email"
+                  placeholder="Email"
                 />
               </div>
               <div className="mb-6">
@@ -39,17 +75,14 @@ const Login = () => {
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="password"
+                  name="password"
                   type="password"
-                  placeholder="******************"
+                  placeholder="*************"
                 />
               </div>
+              {myError && <p>{myError}</p>}
               <div className="flex items-center justify-between">
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="button"
-                >
-                  Sign In
-                </button>
+              <input className='bg-red-400 md:text-lg mt-5 text-white w-[30%] md:w-[20%] mx-auto py-1 cursor-pointer hover:bg-red-600 rounded-md' type="submit" value="Login" />
                 <Link
                   className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
                   href="#"
@@ -64,7 +97,7 @@ const Login = () => {
         <h1 className="text-center mt-5 text-semibold border border-b-2 border-x-0 border-t-0 w-1/4 mx-auto">Or</h1>
 
         <div className="text-center mt-5">
-            <button className="bg-white p-3 shadow-md rounded-md"><FontAwesomeIcon className="text-blue-500" icon={faGoogle} /> Join With Google</button>
+            <button onClick={handleGoogleSignIn} className="bg-white p-3 shadow-md rounded-md"><FontAwesomeIcon className="text-blue-500" icon={faGoogle} /> Join With Google</button>
         </div>
         <div className="text-center mt-1">
             <button className="bg-white p-3 shadow-md rounded-md"><FontAwesomeIcon className="text-blue-500" icon={faFacebook} /> Join With Facebook</button>
--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from '../../../images/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars,faX } from '@fortawesome/free-solid-svg-icons'
+import {onAuthStateChanged,signOut } from "firebase/auth";
+import auth from "../../../firebase.init";
 
 const Header = () => {
     const [menuIcon,setMenuIcon] = useState(false);
+    const [user,setUser] = useState({});
+    useEffect(()=>{
+      onAuthStateChanged(auth, user=>{
+          setUser(user);
+      })
+  },[])
+
+  const handleSignOut =()=>{
+    signOut(auth).then(() => {
+    }).catch((error) => {
+    });
+  }
   return (
     <>
      <nav className="py-5 fixed top-0 left-0 right-0 bg-gray-500/[.3]">
@@ -20,9 +34,10 @@ const Header = () => {
             <Link to='/learn' className="mx-2 my-font hover:text-red-600 hover:underline hover:decoration-2">Learn</Link>
             <Link to='/achievements' className="mx-2 my-font hover:text-red-600 hover:underline hover:decoration-2">Achievements</Link>
             <Link to='/blog' className="mx-2 my-font hover:text-red-600 hover:underline hover:decoration-2">Blog</Link>
+            {user && <button onClick={handleSignOut} className="mx-2 my-font hover:text-red-600 hover:underline hover:decoration-2">Signout</button>}
            </ul>
            <div>
-            <Link to='/login' className="my-font hover:bg-red-600 hover:text-white px-2 py-1 rounded-md">Login</Link>
+            {!user && <Link to='/login' className="my-font hover:bg-red-600 hover:text-white px-2 py-1 rounded-md">Login</Link>}
            </div>
            </div>
         </div>
@@ -35,12 +50,13 @@ const Header = () => {
             <button className="mr-5 text-xl text-white" onClick={()=>setMenuIcon(!menuIcon)}>
                 {menuIcon ?  <FontAwesomeIcon icon={faX} /> :<FontAwesomeIcon icon={faBars} />}
             </button>
-            <ul className={`pb-12 absolute flex flex-col bg-gray-600/[.06] border border-red-100 border-b-0 border-r-0 border-l-0  z-[-1] top-[67px] w-full  pl-9 transition-all duration-500 ease-in ${menuIcon ? 'left-0 ':'left-[-890px]'}`}>
-            <Link to='/' className="my-font text-white hover:text-red-600 hover:underline hover:decoration-2 mt-4 mb-2 w-[100%] ">Home</Link>
-            <Link to='/learn' className="my-font text-white hover:text-red-600 hover:underline hover:decoration-2 my-2 w-[100%] ">Learn</Link>
-            <Link to='/achievements' className="my-font text-white hover:text-red-600 hover:underline hover:decoration-2 my-2 w-[100%] ">Achievements</Link>
-            <Link to='/blog' className="my-font text-white hover:text-red-600 hover:underline hover:decoration-2 my-2 w-[100%] ">Blog</Link>
-            <Link to='/login' className="my-font text-white hover:text-red-600 hover:underline hover:decoration-2 my-2 w-[100%]">Login</Link>
+            <ul className={`pb-12 absolute flex flex-col bg-gray-600/[.5] border border-red-100 border-b-0 border-r-0 border-l-0  z-[-1] top-[67px] w-full  pl-9 transition-all duration-500 ease-in ${menuIcon ? 'left-0 ':'left-[-890px]'}`}>
+            <Link to='/' className="my-font text-sky-300 hover:text-red-600 hover:underline hover:decoration-2 mt-4 mb-2 w-[100%] ">Home</Link>
+            <Link to='/learn' className="my-font text-sky-300 hover:text-red-600 hover:underline hover:decoration-2 my-2 w-[100%] ">Learn</Link>
+            <Link to='/achievements' className="my-font text-sky-300 hover:text-red-600 hover:underline hover:decoration-2 my-2 w-[100%] ">Achievements</Link>
+            <Link to='/blog' className="my-font text-sky-300 hover:text-red-600 hover:underline hover:decoration-2 my-2 w-[100%] ">Blog</Link>
+            {user && <Link onClick={handleSignOut} className="my-font text-sky-300 hover:text-red-600 hover:underline hover:decoration-2 my-2 w-[100%] ">Signout</Link>}
+            {!user && <Link to='/login' className="my-font text-sky-300 hover:text-red-600 hover:underline hover:decoration-2 my-2 w-[100%]">Login</Link>}
              </ul>
            </div>
         </div>
